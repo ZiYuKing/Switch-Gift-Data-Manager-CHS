9df46deb466574410620d4271e70cf20b444a7fe
ZiYu-King <103927505+ZiYuKing@users.noreply.github.com>
2023-07-29 19:56:31 +0800

    Fix known bugs

--- SwitchGiftDataManager.Core/Classes/BCATManager/BCATManager.cs
+++ SwitchGiftDataManager.Core/Classes/BCATManager/BCATManager.cs
@@ -136,7 +136,7 @@ public class BCATManager
         if (WCList is not null && WCList.Count > 0)
             foreach (var wc in WCList)
             {
-                var str = wc.Type!.ToString()!.Equals("宝可梦") ? ((PokemonGift)wc.Content!).GetSpeciesName().Replace("\r","") : wc.Type!.ToString()!;
+                var str = wc.Type!.ToString()!.Equals("Pokemon") ? ((PokemonGift)wc.Content!).GetSpeciesName().Replace("\r","") : wc.Type!.ToString()!;
                 list.Add($"{Game} #{wc.WCID}: {str}");
             }
         return list;
@@ -285,7 +285,7 @@ public class BCATManager
     private static string ForgeWcFileName(Wondercard wc)
     {
         var type = wc.Type!.ToString()!;
-        var content = type.Equals("宝可梦") ? $"{((PokemonGift)wc.Content!).GetSpeciesName().Replace("\r", "")}" : type;
+        var content = type.Equals("Pokemon") ? $"{((PokemonGift)wc.Content!).GetSpeciesName().Replace("\r", "")}" : type;
         var str = $"{wc.WCID:0000}_{content}";
         var sb = new System.Text.StringBuilder();
         foreach (char c in str)
